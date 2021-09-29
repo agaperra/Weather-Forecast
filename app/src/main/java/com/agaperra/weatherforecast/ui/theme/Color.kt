ae938f5272bd59bbd252075d6b100cdb3c6f1fe0
agaperra <agaperra@yandex.ru>
2021-09-29 22:44:45 +0300

    adding colors to compose resources

--- app/src/main/java/com/agaperra/weatherforecast/ui/theme/Color.kt
+++ app/src/main/java/com/agaperra/weatherforecast/ui/theme/Color.kt
@@ -6,3 +6,9 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+//UI colors
+val firstGrayBlue = Color(0xFF393E5B)
+val secondOrangeDawn = Color(0xFFFF6053)
+val secondaryLightCarrot = Color(0xFFFFA957)
+val secondaryPearlWhite = Color(0xFFEDE6CB)
