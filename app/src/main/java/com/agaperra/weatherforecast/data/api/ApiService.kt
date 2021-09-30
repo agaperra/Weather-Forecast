367295d7de0f9f01a816e3a3de7e26af6ead814c
agaperra <agaperra@yandex.ru>
2021-09-30 18:26:41 +0300

    the first stage of receiving a request from api

---
+++ app/src/main/java/com/agaperra/weatherforecast/data/api/ApiService.kt
@@ -0,0 +1,18 @@
+package com.agaperra.weatherforecast.data.api
+
+import ForecastResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET(value = "/v1/forecast.json")
+    suspend fun getForecast(
+        @Query("key") key :String,
+        @Query("q") q: String,
+        @Query("days") days: Int,
+        @Query("aqi") aqi: String,
+        @Query("alerts") alerts: String
+
+    ) : ForecastResponse
+}
---
+++ app/src/main/java/com/agaperra/weatherforecast/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.agaperra.weatherforecast.utils
+
+class Constants {
+    companion object{
+        const val WEATHER_API_URL = "http://api.weatherapi.com"
+    }
+}
