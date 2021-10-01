e3cc46ae57c034e6dc9a9a503fc481028fabb87a
Варвара Агапова <agaperra@yandex.ru>
2021-10-01 18:03:52 +0300

    add files

---
+++ app/src/main/java/com/agaperra/weatherforecast/utils/AppThemes.kt
@@ -0,0 +1,37 @@
+package com.agaperra.weatherforecast.utils
+
+import androidx.annotation.DrawableRes
+import androidx.compose.ui.graphics.Color
+import com.agaperra.weatherforecast.R
+import com.agaperra.weatherforecast.ui.theme.Color.firstGrayBlue
+
+
+sealed class AppThemes(
+    @DrawableRes val backgroundRes: Int,
+    val textColor: Color,
+    val iconsTint: Color?
+) {
+    data class RainyTheme(
+        @DrawableRes val background: Int = R.drawable.background_rainy,
+        val color: Color = firstGrayBlue,
+        val tint: Color? = firstGrayBlue
+    ) : AppThemes(backgroundRes = background, textColor = color, iconsTint = tint)
+
+    data class CloudyTheme(
+        @DrawableRes val background: Int = R.drawable.background_cloudy,
+        val color: Color = Color.White,
+        val tint: Color? = Color.White
+    ) : AppThemes(backgroundRes = background, textColor = color, iconsTint = tint)
+
+    data class WindyTheme(
+        @DrawableRes val background: Int = R.drawable.background_windy,
+        val color: Color = firstGrayBlue,
+        val tint: Color? = firstGrayBlue
+    ) : AppThemes(backgroundRes = background, textColor = color, iconsTint = tint)
+
+    data class SunnyTheme(
+        @DrawableRes val background: Int = R.drawable.background_sunny,
+        val color: Color = Color.White,
+        val tint: Color? = Color.White
+    ) : AppThemes(backgroundRes = background, textColor = color, iconsTint = tint)
+}
