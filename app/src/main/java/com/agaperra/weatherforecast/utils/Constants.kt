f5140ba0722d7a1544d8568adbfb12e83b983771
agaperra <agaperra@yandex.ru>
2021-10-04 13:45:24 +0300

    updated codes for themes

--- app/src/main/java/com/agaperra/weatherforecast/utils/Constants.kt
+++ app/src/main/java/com/agaperra/weatherforecast/utils/Constants.kt
@@ -15,19 +15,13 @@ object Constants {
     const val WEATHER_API_URL = "https://api.openweathermap.org"
     const val MINIMUM_DISTANCE_FOR_UPDATES: Float = 10F // в метрах
     const val MINIMUM_TIME_BETWEEN_UPDATES: Long = 2000L // в мс
-    // коды для тем
-    val cloudy_theme = arrayListOf<Int>(1003, 1006, 1009, 1063, 1066, 1069, 1072)
-    val sunny_theme = arrayListOf<Int>(1000, 1240, 1243, 1249, 1252)
-    val rainy_theme = arrayListOf<Int>(1150, 1153, 1168, 1171, 1180, 1183, 1186, 1189, 1192, 1195, 1198, 1201)
-    val thunder_theme = arrayListOf<Int>(1087, 1246, 1273, 1276, 1279, 1282)
-    val foggy_theme = arrayListOf<Int>(1030, 1114, 1117, 1135, 1147)
-    val snow_theme = arrayListOf<Int>(1204, 1207, 1210, 1213, 1216, 1219, 1222, 1225, 1237, 1255, 1258, 1261, 1264)
 
-    // коды для иконок
-    val cloudy_icons = arrayListOf<Int>(119, 122, 311)
-    val sunny_icons = arrayListOf<Int>(113, 116)
-    val rainy_icons = arrayListOf<Int>(176, 263, 266, 281, 284, 293, 296, 299, 302, 305, 308, 314, 353, 356,)
-    val thunder_icons = arrayListOf<Int>(200, 359, 386, 389, 392, 395)
-    val snow_icons = arrayListOf<Int>(179,182, 317, 320, 323, 326, 329, 332, 335, 338, 350, 362, 365, 368, 371, 374, 377)
-    val mist_icons = arrayListOf<Int>(143, 185, 227, 230, 248, 260)
+    val clouds = arrayListOf<Int>(801, 802, 803, 804)
+    val sunny = arrayListOf<Int>(800)
+    val rain = arrayListOf<Int>(500, 501, 502, 503, 504, 511, 520, 521, 522, 531)
+    val drizzle = arrayListOf<Int>(300, 301, 302, 310, 311, 312, 313, 314, 321)
+    val thunderstorm = arrayListOf<Int>(200, 201, 202, 210, 211, 212, 221, 230, 231, 232)
+    val atmosphere = arrayListOf<Int>(701, 711, 721, 731, 741, 751, 761, 762, 771, 781)
+    val snow = arrayListOf<Int>(600, 601, 602, 611, 612, 613, 615, 616, 620, 621, 622)
+
 }
